ea70cff608e124f2497244dac3e44b67b5ad8c6c
Wanda <cwand07@gmail.com>
2022-04-11 18:26:02 -0300

    Clean Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,5 @@
 import { createStore } from 'vuex';
 
-//  import axios from 'axios';
-
 export default createStore({
   state: {
     products: [],
@@ -43,36 +41,6 @@ export default createStore({
         console.log(error);
       }
     },
-
-    // async created() {
-    //   let url = "https://fakestoreapi.com/products";
-
-    //   await axios
-    //     .get(url)
-    //     .then((response) => (this.products = response.data))
-    //     .then((response) => console.log(response));
-    // },
-
-    // created() {
-    //   axios.get("/api/services").then(response => {
-    //     this.users = response.data;
-    //   });
-    // }
-    // };
-
-    // Fetch nos permite pasar como parametro la url de la api a consumir / Agregar un segundo parametro, pero si no lo agregamos automaticamente crea una peticion GET
-    // let url = "https://62435d9539aae3e3b7422c44.mockapi.io/api/v1/users";
-    // fetch(url)
-    // .then((response) => response.json())
-    // Para guardar la respuesta en el objeto posts tengo que concatenar otra promesa que me guarde la data obtenida
-    // .then((data) => (this.posts = data));
-
-    // async created() {
-    //     let url = 'https://jsonplaceholder.typicode.com/posts';
-    //     const response = await fetch(url);
-    //     const postData = await response.json();
-    //     this.posts = postData;
-    // }
 
     filterByStatus({ commit, state }, status) {
       const results = state.products.filter((product) => {
