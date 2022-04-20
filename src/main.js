fa944b7bc28d6270bb6d3c1657006d59cd388908
Wanda <cwand07@gmail.com>
2022-04-20 14:51:22 -0300

    Uninstall Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap-vue/dist/bootstrap-vue.css';
-
 import App from './App.vue';
-// import { BootstrapVue } from 'bootstrap-vue';
 import { createApp } from 'vue';
 import router from './router/router';
 import store from './store';
 
 createApp(App).use(router).use(store).mount('#app');
-// createApp(App).use(BootstrapVue).use(router).use(store).mount('#app');
 
 // import { createPinia } from 'pinia';
 // createApp(App).use(router).use(createPinia()).mount('#app');
